838e0c3fa630652265cfa533ec7d156c3bc15228
Ludovic Claude <ludovic.claude@laposte.net>
2013-03-20 01:17:21 +0100

    Getting inspiration from Pivotal Tracker tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,24 @@
+/*jshint debug:true, node:true */
+
 //Gruntfile.js
 
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-crx');
+	
+    // -- LOAD GRUNT PLUGINS -- //
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-crx');
+
+    // --- HELPERS --- //
+    var glg = grunt.log.write;
+    glg('Starting');
+    debugger;
+
+    // --- CREATE CONFIGURATION --- //
+    var config = {
+       pkg: grunt.file.readJSON('package.json'),
+       manifest: grunt.file.readJSON('extension/manifest.json')
+    };
 
 	grunt.initConfig({
 	  manifest: grunt.file.readJSON('src/manifest.json'),
