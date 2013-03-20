397f690531d962039f71cf8e887ab0c3a927e576
Ludovic Claude <ludovic.claude@laposte.net>
2013-03-21 00:49:43 +0100

    Fix Gruntfile config

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,9 @@
 //Gruntfile.js
 
 module.exports = function(grunt) {
-	
-    // -- LOAD GRUNT PLUGINS -- //
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-crx');
+    
+    // -- LOAD ALL GRUNT PLUGINS -- //
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // --- HELPERS --- //
     var glg = grunt.log.write;
@@ -15,66 +13,69 @@ module.exports = function(grunt) {
     debugger;
 
     // --- CREATE CONFIGURATION --- //
-    var config = {
-       pkg: grunt.file.readJSON('package.json'),
-       manifest: grunt.file.readJSON('extension/manifest.json')
-    };
-
-	grunt.initConfig({
-		
+    grunt.initConfig({
+        
       // specify an alternate install location for Bower
       bower: {
         dir: 'src/vendor'
       },
-		
-	  manifest: grunt.file.readJSON('src/manifest.json'),
-	  
+        
+      pkg: grunt.file.readJSON('package.json'),
+      manifest: grunt.file.readJSON('src/manifest.json'),
+      
       jshint: {
-		options: {
-			jshintrc: '.jshintrc'
-		},
+        options: {
+            jshintrc: '.jshintrc'
+        },
         build : ['Gruntfile.js'],
-		app: [
-			'src/{,*/}*.js',
-			'!src/vendor/*',
-			'test/spec/{,*/}*.js'
-		]
-	  },
-	  
-	  crx: {
-	    staging: {
-	      "src": "src/",
-	      "dest": "dist/staging/src",
-	      "baseURL": "http://my.app.intranet/files/",
-	      "filename": "<%= pkg.name %>-<%= manifest.version %>-dev.crx",
-	      "exclude": [ ".git", ".svn", "*.pem" ],
-	      "options": {
-	        "maxBuffer": 3000 * 1024 //build extension with a weight up to 3MB
-	      }
-	    },
-	    production: {
-	      "src": "src/",
-	      "dest": "dist/production/src",
-	      "baseURL": "http://my.app.net/files/",
-	      "exclude": [ ".git", ".svn", "dev/**", "*.pem" ],
-	      "privateKey": "/.ssh/chrome-apps.pem",
-	      "options": {
-	        "maxBuffer": 3000 * 1024 //build extension with a weight up to 3MB
-	      }
-	    }
-	  },
-	  
+        app: [
+            'src/{,*/}*.js',
+            '!src/vendor/*',
+            'test/spec/{,*/}*.js'
+        ]
+      },
+      
+      crx: {
+        staging: {
+          "src": "src/",
+          "dest": "dist/staging/src",
+          "baseURL": "http://my.app.intranet/files/",
+          "filename": "<%= pkg.name %>-<%= manifest.version %>-dev.crx",
+          "exclude": [ ".git", ".svn", "*.pem" ],
+          "options": {
+            "maxBuffer": 3000 * 1024 //build extension with a weight up to 3MB
+          }
+        },
+        production: {
+          "src": "src/",
+          "dest": "dist/production/src",
+          "baseURL": "http://my.app.net/files/",
+          "exclude": [ ".git", ".svn", "dev/**", "*.pem" ],
+          "privateKey": "/.ssh/chrome-apps.pem",
+          "options": {
+            "maxBuffer": 3000 * 1024 //build extension with a weight up to 3MB
+          }
+        }
+      },
+      
       /** WATCH:
        *  provide a fast development watch and a slower full everything watch.
        */
-      watch = {
-		full : {
-			files : ['Gruntfile.js', 'src/*.json',
-					'src/*.js'],
-			tasks : ['build']
-	    }
-	  }
-	  
-	});
-}
+      watch: {
+        full : {
+            files : ['Gruntfile.js', 'src/*.json',
+                    'src/*.js'],
+            tasks : ['build']
+        }
+      }
+      
+    });
+    
+    grunt.registerTask('build',   ['jshint', 'crx']);
+    grunt.registerTask('dev',     ['build', 'watch:full']);
+    grunt.registerTask('default', ['dev']);
+
+    grunt.loadNpmTasks('grunt-devtools');
+    
+};
 
